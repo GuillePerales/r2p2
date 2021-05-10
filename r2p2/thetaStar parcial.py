3ba0817c25f1b6308be301fb9d37b8eb22e1d7ca
ivancanteromartin <78930579+Ivan-Cantero-Martin@users.noreply.github.com>
2021-05-10 10:29:38 +0200

    Starting thetha* and lineOfSight functions

--- r2p2/thetaStar parcial.py
+++ r2p2/thetaStar parcial.py
@@ -1,4 +1,5 @@
 
+import math
 import path_planning as pp
 
 def children(point,grid):
@@ -50,3 +51,30 @@ def children(point,grid):
     return [link for link in links if link.value != 9]
 
 
+openset = set()
+
+def theta_Star(start, goal, grid, heuristic):
+    """
+    start.G = 0
+    start.H = pp.heuristic[heuristic](start,goal)
+    start.parent = start
+    abiertos = set()
+    abiertos.add(start, start.G + start.H)
+    """
+    closedset = set()
+    current = start #!!!!!
+    openset.add(start)
+    while openset:
+
+
+pp.register_search_method('theta*', theta_Star)
+
+
+def lineOfSight(current, node, grid):
+    x0, y0 = current.grid_point
+    x1, y1 = node.grid_point
+    dy = y1 - y0
+    dx = x1 - x0
+    sx = 0
+    sy = 0
+    f = 0
